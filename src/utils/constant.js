0d5ef00cf5fedb46cac9eec4b79b7f84c2418af3
thanhquang2004 <quangthanh7204@gmail.com>
2025-02-21 22:30:53 +0700

    Fix CORS and build modes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,13 @@
-export const API_ROOT = "http://localhost:8080";
+/* eslint-disable no-undef */
+// export const API_ROOT = "http://localhost:8080";
+let apiRoot = "";
+
+if (process.env.BUILD_MODE === "dev") {
+  apiRoot = "http://localhost:8080";
+}
+
+if (process.env.BUILD_MODE === "production") {
+  apiRoot = "https://trello-api-hg29.onrender.com";
+}
+
+export const API_ROOT = apiRoot;
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
+/* eslint-disable no-undef */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "process.env": process.env,
+  },
+
   plugins: [react(), svgr()],
   //base: './
   resolve: {
